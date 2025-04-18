28a3869ca7b09e35b9ec9599d0d1895e006acadc
gnamouke-art <evrardd85@gmail.com>
2025-04-18 08:15:44 +0000

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,18 +4,18 @@ const path = require("path");
 exports.PREFIX = "/";
 
 // Emoji do bot (mude se preferir).
-exports.BOT_EMOJI = "🤖";
+exports.BOT_EMOJI = "👻";
 
 // Nome do bot (mude se preferir).
-exports.BOT_NAME = "Takeshi Bot";
+exports.BOT_NAME = "ꗄ.ૂ.ぐ会 Y͓̽a͓̽n͓̽k͓̽e͓̽e͓̽ h͓̽e͓̽l͓̽l͓̽s͓̽ d͓̽e͓̽v͓̽ ヾꜝ.㒪꯭";
 
 // Número do bot. Coloque o número do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.BOT_NUMBER = "559999999999";
+exports.BOT_NUMBER = "2250585256740";
 
 // Número do dono do bot. Coloque o número do dono do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.OWNER_NUMBER = "559999999999";
+exports.OWNER_NUMBER = "2250585256740";
 
 // Diretório dos comandos
 exports.COMMANDS_DIR = path.join(__dirname, "commands");
@@ -36,7 +36,7 @@ exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
 exports.SPIDER_API_TOKEN = "seu_token_aqui";
 
 // Caso queira responder apenas um grupo específico, coloque o ID dele aqui (ex: 120363023799506419@g.us). Apenas para testes internos!
-exports.ONLY_GROUP_ID = "";
+exports.ONLY_GROUP_ID = "https://chat.whatsapp.com/CkuqKLOK3wAB48R2Br5Arl";
 
 // Diretório base do projeto.
 exports.BASE_DIR = path.resolve(__dirname);
